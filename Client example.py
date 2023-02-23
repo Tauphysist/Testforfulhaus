a62f068d8742bf5331e824e550608b235e9503e1
Tayphysist <43246449+Tauphysist@users.noreply.github.com>
2023-02-22 22:48:34 -0500

    Final changes

--- Client example.py
+++ Client example.py
@@ -1,14 +1,15 @@
+#Importing Libraries
 import requests
 import io
 from PIL import Image
 
-
+#preparing image to be sent
 data=Image.open('./Modelcheck/Adara Desk Chair-Knoll Natural.jpg')
 new_image = data.resize((180, 180))
-
 buf = io.BytesIO()
 new_image.save(buf, format='JPEG')
 byte_im = buf.getvalue()
+#image MUST be sent as binaey
 r = requests.post('https://tauphys.pythonanywhere.com/predict_api', data=byte_im)
-
+#request may take some time to process
 print(r.text)
